319c420a80af5353c73f7116b0d1e277ac529933
HarutoMizuno <koretteZIYUUya2024@gmail.com>
2025-10-21 09:27:44 +0900

    style-changed-1
    各所UI/UXならびにユーザーアクセシビリティ・キーボードインターフェースを改善した。UI・UX等スマホ画面表示での監査ならびにクライアントへの共有のために公開する。

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ jQuery(window).on('load', function(){
     }
 
     setTimeout(function() {
-    jQuery('.l-loadingPage').addClass('l-loadingPage__is-active');
+    jQuery('.l-loadingPage').addClass('l-loadingPage__is-hidden');
 
     jQuery('.l-mainPage').fadeIn(2000); 
     },2000);
@@ -28,33 +28,35 @@ jQuery(window).on('load', function(){
             const scrollPos = jQuery(window).scrollTop();
             const windowHeight = jQuery(window).height();
 
-            if(width < 750){
-                if (scrollPos > 300) {
+            if(width < 750)/*スマホ用*/{
+                //下層ページ
+                if (scrollPos > 100) {
                     jQuery('.l-header__subpage').addClass('l-header__subpage__is-open');
-                }
-                else {
-                    jQuery('.l-header__subpage').removeClass('l-header__subpage__is-open');
+                    jQuery('.nav__sp').css({
+                        'background-color': 'transparent',
+                        'box-shadow': 'none'
+                    })
+                }else{
+                    jQuery('.nav__sp').css({
+                        'background-color': 'white',
+                        'box-shadow': '0 4px 8px rgba(0, 0, 0, 0.3)'
+                    })
                 }
 
-                if (scrollPos > 700) {
+                //トップページ
+                if (scrollPos > 400) {
                     jQuery('.l-header').addClass('l-header__is-open');
                 }
-                else {
-                    jQuery('.l-header').removeClass('l-header__is-open');
-                }
-            }else{
-                if (scrollPos > 500) {
+                
+            }else/*PC用*/{
+                //下層ページ
+                if (scrollPos > 200) {
                     jQuery('.l-header__subpage').addClass('l-header__subpage__is-open');
                 }
-                else {
-                    jQuery('.l-header__subpage').removeClass('l-header__subpage__is-open');
-                }
             
-                if (scrollPos > 800) {
+                //トップページ
+                if (scrollPos > 500) {
                     jQuery('.l-header').addClass('l-header__is-open');
-                }
-                else {
-                    jQuery('.l-header').removeClass('l-header__is-open');
                 }
             }
 
@@ -75,14 +77,61 @@ jQuery(window).on('load', function(){
             
         });
 
-    jQuery('.navSp_hamburger').click(
-        function(){
-            jQuery('.line1').toggleClass('line1__is-active');
-            jQuery('.line2').toggleClass('line2__is-active');
-            jQuery('.line3').toggleClass('line3__is-active');
-            jQuery('.navSp_dropdown').toggleClass('navSp_dropdown__is-open');
+    jQuery('.skipLink_text').focus(
+        function() {
+            jQuery('.skipLink').addClass('skipLink__is-focused');
+            jQuery('.l-header__subpage').addClass('l-header__subpage__is-open');
+            jQuery('.l-header').addClass('l-header__is-open');
+        }
+    ).blur(
+        function() {
+            jQuery('.skipLink').removeClass('skipLink__is-focused');
         }
     );
+    
+    jQuery('.navPc_item__home').focus(
+        function() {
+            jQuery('.navPc_home').addClass('navPc_home__is-focused');
+        }
+    ).blur(
+        function() {
+            jQuery('.navPc_home').removeClass('navPc_home__is-focused');
+        }
+    );
+
+    jQuery('.navSp_hamburger').click(
+        function(){
+            jQuery('.line1').addClass('line1__is-active');
+            jQuery('.line2').addClass('line2__is-active');
+            jQuery('.line3').addClass('line3__is-active');
+            jQuery('.navSp_dropdown').addClass('navSp_dropdown__is-open');
+        }
+    );
+    jQuery('.navSp_hamburger').keydown(
+        function(){
+            jQuery('.line1').addClass('line1__is-active');
+            jQuery('.line2').addClass('line2__is-active');
+            jQuery('.line3').addClass('line3__is-active');
+            jQuery('.navSp_dropdown').addClass('navSp_dropdown__is-open');
+        }
+    );
+
+    jQuery('.dropdown_itemText__close').click(
+        function(){
+            jQuery('.line1').removeClass('line1__is-active');
+            jQuery('.line2').removeClass('line2__is-active');
+            jQuery('.line3').removeClass('line3__is-active');
+            jQuery('.navSp_dropdown').removeClass('navSp_dropdown__is-open');
+        }
+    );
+    jQuery('.dropdown_itemText__close').keydown(
+        function(){
+            jQuery('.line1').removeClass('line1__is-active');
+            jQuery('.line2').removeClass('line2__is-active');
+            jQuery('.line3').removeClass('line3__is-active');
+            jQuery('.navSp_dropdown').removeClass('navSp_dropdown__is-open');
+        }
+    )
 
     jQuery('.navSp_contact').click(
         function(){
